8efe914825e73255d8cf17f630150b6908e4b68a
Mike Garegnani <systems.support@gmail.com>
2021-01-26 08:03:22 -0700

    Replace running icon

--- lib/ground_control_web/components/repository_component.ex
+++ lib/ground_control_web/components/repository_component.ex
@@ -9,8 +9,9 @@ defmodule GroundControlWeb.RepositoryComponent do
         <div class="flex-initial">
           <%= case @repo.status do %>
             <% "running" -> %>
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="animate-ping text-yellow-500 w-8 h-8 flex-shrink-0 mr-4" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              <svg class="animate-spin text-gray-500 w-8 h-8 flex-shrink-0 mr-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
               </svg>
 
             <% "failure" -> %>
